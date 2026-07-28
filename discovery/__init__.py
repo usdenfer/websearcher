df9bfacae57b2ad881a563a7913cd0c8637878f8
Shieker <ezioylq@hotmail.com>
2026-07-28 23:17:05 +0800

    feat: add discovery models and budget

---
+++ discovery/__init__.py
@@ -0,0 +1,15 @@
+from .models import (
+    BudgetManager,
+    Candidate,
+    DiscoveryStats,
+    DomainPolicy,
+    SearchSpec,
+)
+
+__all__ = [
+    "BudgetManager",
+    "Candidate",
+    "DiscoveryStats",
+    "DomainPolicy",
+    "SearchSpec",
+]
